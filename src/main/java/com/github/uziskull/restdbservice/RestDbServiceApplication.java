dcd88be7caf7b1d380f44fc3081acd1b3b83b3c6
Uziskull <jtunes_9@hotmail.com>
2023-11-28 01:40:22 +0000

    Initial Springboot setup

---
+++ src/main/java/com/github/uziskull/restdbservice/RestDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.uziskull.restdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/uziskull/restdbservice/RestDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.uziskull.restdbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
